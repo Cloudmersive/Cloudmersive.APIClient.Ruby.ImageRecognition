f1d49079f394450f05aa020b04ccc4dcf7788b9a
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-09-03 18:37:40 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -1,11 +1,11 @@
 ﻿Remove-Item –path ./client –recurse
 Remove-Item *.gem
 Remove-Item ./client/*.gem
-& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/convert -l ruby -o client -c packageconfig.json
+& java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/swagger/api/image -l ruby -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 
-(Get-Content ./client/cloudmersive-convert-api-client.gemspec).replace('`find *`.split("\n").uniq.sort.select{|f| !f.empty? }', "Dir['./**/*']") | Set-Content ./client/cloudmersive-convert-api-client.gemspec
+(Get-Content ./client/cloudmersive-image-recognition-api-client.gemspec).replace('`find *`.split("\n").uniq.sort.select{|f| !f.empty? }', "Dir['./**/*']") | Set-Content ./client/cloudmersive-image-recognition-api-client.gemspec
 #& npm build ./client
 
 
@@ -14,6 +14,6 @@ Remove-Item ./client/*.gem
 Push-Location
 & cd ./client
 
-& gem build ./cloudmersive-convert-api-client.gemspec
+& gem build ./cloudmersive-image-recognition-api-client.gemspec
 
 Pop-Location
--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,1 @@
-& gem push .\client\cloudmersive-convert-api-client-1.2.9.gem
+& gem push .\client\cloudmersive-image-recognition-api-client-1.2.9.gem
